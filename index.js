b1cc21bcb9cb13527d9b0c995492af10c9728c26
Anton Konstantynov <batann1990@gmail.com>
2024-01-26 22:04:24 +0200

    final

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const balances = [
+  "$123.10",
+  "$61 123.23",
+  "$1,321,123.10",
+  "$0.00",
+  "$799.50",
+  "$1 916 161 123.25",
+];
+
+const balanceNums = balances.map((balance) =>
+  Number(balance.match(/\d*\.?\d+/g).join(""))
+);
+console.log(balanceNums);
